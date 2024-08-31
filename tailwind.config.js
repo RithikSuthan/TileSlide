7c85190444853923c4be79913c2796785402de3b
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-08-31 20:20:29 +0530

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  content: [
+    "./src/**/*.{html,ts}", // Include all HTML and TypeScript files in the src directory
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
